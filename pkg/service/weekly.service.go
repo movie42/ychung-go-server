74edd736b29a64201d1217c400176f3b92a16db2
A <movie3342@gmail.com>
2022-10-24 11:38:54 +0900

    cache reset

--- pkg/service/weekly.service.go
+++
@@ -1,9 +0,0 @@
-package service
-
-import (
-	"github.com/movie42/ychung-go-server/presenter"
-)
-
-func (s *service) FetchWeeklies() (*[]presenter.Weekly, error) {
-	return s.repository.ReadWeeklies()
-}
